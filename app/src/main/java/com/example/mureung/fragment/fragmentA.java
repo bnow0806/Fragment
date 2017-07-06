76cd45b7e706b601b7acb3d6b65e45653a4e185c
bnow0806 <guswp1993@naver.com>
2017-07-06 22:26:01 +0900

    7.6

--- app/src/main/java/com/example/mureung/fragment/fragmentA.java
+++ app/src/main/java/com/example/mureung/fragment/fragmentA.java
@@ -1,45 +1,77 @@
 package com.example.mureung.fragment;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class fragmentA extends Fragment implements OnMapReadyCallback {
+public class fragmentA extends Fragment{
 
-    GoogleMap gMap;
+    MapView   mMapView;
+    private GoogleMap gMap;
+
     double lat = 37.541;
     double lng = 126.986;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragmenta, container, false);
 
-
-        SupportMapFragment gMap = (SupportMapFragment)view.getSupportMapFragmentManager().findFragmentById(R.id.map);
-        gMap.getMapAsync(this);
-
-        return view;
-    }
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+                 mMapView.onCreate(savedInstanceState);
+               mMapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        mMapView.getMapAsync(new OnMapReadyCallback() {
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap mMap) {
+        googleMap=mMap;
+        googleMap.setMyLocationEnabled(true);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15));
         MarkerOptions marker = new MarkerOptions();
         marker.position(new LatLng(lat, lng));
         googleMap.addMarker(marker).showInfoWindow();
-
     }
-
-
+        });
+    return view;
+    }
+      @Override
+         public void onResume() {
+             super.onResume();
+             mMapView.onResume();
+         }
+         @Override
+         public void onPause() {
+             super.onPause();
+             mMapView.onPause();
+         }
+         @Override
+         public void onDestroy() {
+             super.onDestroy();
+             mMapView.onDestroy();
+         }
+         @Override
+         public void onLowMemory() {
+             super.onLowMemory();
+             mMapView.onLowMemory();
+       }
 }
