1a976c282162bd392952eda47cc2273d7d842fa3
bnow0806 <guswp1993@naver.com>
2017-07-10 17:09:38 +0900

    7.10

--- app/src/main/java/com/example/mureung/fragment/fragmentA.java
+++ app/src/main/java/com/example/mureung/fragment/fragmentA.java
@@ -32,41 +32,35 @@ public class fragmentA extends Fragment{
         final View view = inflater.inflate(R.layout.fragmenta, container, false);
 
         mMapView = (MapView) view.findViewById(R.id.mapView);
-                 mMapView.onCreate(savedInstanceState);
-               mMapView.onResume();
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         mMapView.getMapAsync(new OnMapReadyCallback() {
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
 
-        final TextView lat=(TextView)view.findViewById(R.id.lat);
-        lat.setText("위도: "+a);
-        final TextView lng=(TextView)view.findViewById(R.id.lng);
-        lng.setText("경도: "+b);
+                final TextView lat=(TextView)view.findViewById(R.id.lat);
+                lat.setText("위도: "+a);
+                final TextView lng=(TextView)view.findViewById(R.id.lng);
+                lng.setText("경도: "+b);
 
-        MarkerOptions marker=new MarkerOptions().position( new LatLng(a, b));
+                MarkerOptions marker=new MarkerOptions().position( new LatLng(a, b));
         /*marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.map_icon01));아이콘 바꾸는 코드*/
 
-        mMap.addMarker(marker);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(a, b), 17));
+                mMap.addMarker(marker);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(a, b), 17));
 
-        Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
                 mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
                     @Override
                     public void onMapLongClick(LatLng latLng) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
+                        MarkerOptions markerOptions = new MarkerOptions().position(latLng);
                         mMap.clear();
                         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
                         mMap.addMarker(markerOptions);
@@ -79,17 +73,36 @@ public class fragmentA extends Fragment{
                         Log.e("222","222");
 
                         //lat,lng 보내는 코드, 어차피 수정안했을때는 다시보내줄 필요 x
-
                     }});
-            } });
-    }});
 
+            }});
+        Button button = (Button) view.findViewById(R.id.kakaoshare);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //intent 공유
+             Log.e("111","1111");
+            }
+        });
         return view;
     }
- @Override
+    @Override
     public void onResume() {
         super.onResume();
         mMapView.onResume();
     }
+}
 
-    }
+
+
+
+/*uri intent
+Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+mapIntent.setPackage("com.google.android.apps.maps");
+if (mapIntent.resolveActivity(getPackageManager()) != null) {
+    startActivity(mapIntent);
+}
+ */
