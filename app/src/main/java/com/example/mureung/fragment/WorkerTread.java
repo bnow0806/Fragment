f34690310bc583064ba2a58c50901441335294ac
bnow0806 <guswp1993@naver.com>
2017-07-14 17:43:46 +0900

    7.14

--- app/src/main/java/com/example/mureung/fragment/WorkerTread.java
+++ app/src/main/java/com/example/mureung/fragment/WorkerTread.java
@@ -1,4 +1,8 @@
 package com.example.mureung.fragment;
+import android.os.Handler;
+import android.os.Message;
+
+
 
 /**
  * Created by HyunJe on 2017-07-14.
@@ -6,8 +10,33 @@ package com.example.mureung.fragment;
 //작업 스레드(handler 클래스를 이용)-progressbar
 
 public class WorkerTread extends Thread {
+Handler handler;
 
+    WorkerTread(Handler handler){
+        this.handler=handler;
+    }
+public void run(){
 
-
-
-}
+//i를 0~1000, 1000~0, 0~1000 시뮬레이터
+    for(int i=0;i<1001;i++){
+        try{
+            Thread.sleep(10);//기다리는 함수
+        }catch(Exception e){}
+        Message msg=new Message();
+        msg.arg1=i;
+        handler.sendMessage(msg);}
+    for(int j=1001;j>0;j--){
+        try{
+            Thread.sleep(10);
+        }catch(Exception e){}
+        Message msg=new Message();
+        msg.arg1=j;
+        handler.sendMessage(msg);}
+    for(int k=0;k<1001;k++){
+        try{
+            Thread.sleep(10);
+        }catch(Exception e){}
+        Message msg=new Message();
+        msg.arg1=k;
+        handler.sendMessage(msg);}
+}}
--- app/src/main/java/com/example/mureung/fragment/fragmentB.java
+++ app/src/main/java/com/example/mureung/fragment/fragmentB.java
@@ -1,6 +1,8 @@
 package com.example.mureung.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,18 +12,22 @@ import android.widget.TextView;
 
 
 /**
- * Created by HyunJe on 2017-07-13.
+ * Created by HyunJe on 2017-07-14.
  */
 
 //실시간 대쉬보드, 연료탭 모드
 public class fragmentB extends Fragment {
     double avgfuel = 1560;
 //평균 유류비 초기값 선언
-    private  static  final int PROGRESS=0x1;
-    private ProgressBar mProgress;
+
+    /*private  static  final int PROGRESS=0x1;
     private  int mProgressStatus=0;
-    int i=0;
-    private TextView progress;
+    int i=0;*/ //작업스레드 변수선언
+
+    private ProgressBar mProgress;
+    Handler handler;
+    WorkerTread thread;
+    TextView progress;
    //progressbar 변수 및 초기값 선언
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,15 +38,34 @@ public class fragmentB extends Fragment {
         progress=(TextView)view.findViewById(R.id.progress);
         fuel1.setText(avgfuel+"원");
         //text에 들어갈 값을 데이터로 설정
-
         mProgress=(ProgressBar) view.findViewById(R.id.progress_bar);
+        mProgress.setProgress(0);
+        progress.setText(0.0+"%");//progressbar 초기값 지정
 
-        //progress bar 선언
-        //progress bar 동작 정의-작업스레드 정의
-        new Thread(new Runnable() {
+        //handler class를 이용한 ui스레드 제어
+        handler=new Handler(){
+            public void handleMessage(Message msg){
+
+                mProgress.setProgress(msg.arg1);
+                progress.setText((double)msg.arg1/10+"%");
+            }
+        };
+        thread= new WorkerTread(handler);
+        thread.start();
+return view;
+}}
+
+
+
+
+
+
+
+//progress bar 동작 정의-작업스레드 정의(자체정의 방법)
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
-                //실재 동작 함수
+                //실제 동작 함수
                 while (mProgressStatus<1000){
                     try{
                         Thread.sleep(10);//()시간동안 기다리는 함수
@@ -54,11 +79,10 @@ public class fragmentB extends Fragment {
                         @Override
                         public void run() {
                             mProgress.setProgress(mProgressStatus);
-                            progress.setText((double)mProgressStatus/10+"%");
+                            progress.setText((double)mProgressStatus/10+"%");//progressbar, text 출력
                         }
                     });
                 }
             }
-        }).start();
-return view;
-}}
+        }).start();*/
+
